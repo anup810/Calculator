48901dc678d4d30569aac10ff6d6cb1b1e30b7f3
Anup Saud <anupsaud10@gmail.com>
2025-01-04 20:58:10 -0500

    Add calculator UI layout in storyboard and button setup with action handlers in ViewController

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var roundButtons: [UIButton]!
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    
+    lazy var operationButtons: [UIButton] = [divideButton, multiplyButton, minusButton, plusButton]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupButton()
     }
+    func setupButton(){
+        for button in operationButtons {
+            button.layer.cornerRadius = button.frame.height / 2
+        }
 
-
+        for button in roundButtons{
+            button.layer.cornerRadius = button.frame.height / 2
+        }
+        
+    }
+    
+    @IBAction func didTapOperationButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapNumberButtons(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func didTapDecimalButton() {
+    }
+    
+    @IBAction func didTapEqualsButton() {
+    }
+    
+    @IBAction func didTapPercentageButton() {
+    }
+    
+    @IBAction func didTapPlusMinusButton() {
+    }
+    
+    @IBAction func didTapClearButton() {
+    }
 }
 
