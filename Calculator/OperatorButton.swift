70f11a0af267153fafde7e6414102191a2b7cf53
Anup Saud <anupsaud10@gmail.com>
2025-01-05 08:22:42 -0500

    Refactor operation button handling with deselect logic and improved highlight behavior

---
+++ Calculator/OperatorButton.swift
@@ -0,0 +1,14 @@
+//
+//  OperationButton.swift
+//  Calculator
+//
+//  Created by Anup Saud on 2025-01-05.
+//
+
+import UIKit
+
+class OperatorButton: UIButton {
+    var isSelection = false
+ 
+
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet var roundButtons: [UIButton]!
-    @IBOutlet weak var divideButton: UIButton!
-    @IBOutlet weak var multiplyButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var divideButton: OperatorButton!
+    @IBOutlet weak var multiplyButton: OperatorButton!
+    @IBOutlet weak var minusButton: OperatorButton!
+    @IBOutlet weak var plusButton: OperatorButton!
     
-    lazy var operationButtons: [UIButton] = [divideButton, multiplyButton, minusButton, plusButton]
+    lazy var operationButtons: [OperatorButton] = [divideButton, multiplyButton, minusButton, plusButton]
+    
+    enum Operation{
+        case divide
+        case multiply
+        case minus
+        case plus
+        case none
+    }
+    var operation: Operation = .none
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,11 +42,50 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func didTapOperationButton(_ sender: UIButton) {
+    @IBAction func didTapOperationButton(_ sender: OperatorButton) {
+        let title = sender.currentTitle
+        
+        switch title{
+        case "÷":
+            operation = .divide
+        case "X":
+            operation = .multiply
+        case "-":
+            operation = .minus
+        case "+":
+            operation = .plus
+        default:
+            break
+        }
+        highLightButton(button: sender)
+
+    }
+    func deselectButton(){
+        for button in operationButtons{
+            button.backgroundColor = .systemOrange
+            button.setTitleColor(.white, for: .normal)
+            button.isSelection = false
+        }
+    }
+    func highLightButton(button: OperatorButton){
+        deselectButton()
+        button.backgroundColor = .white
+        button.setTitleColor(.systemOrange, for: .normal)
+        button.isSelection = true
+
     }
     
     @IBAction func didTapNumberButtons(_ sender: UIButton) {
+       let number = sender.tag
+        if displayLabel.text == "0"{
+            displayLabel.text = "\(number)"
+        }else{
+            displayLabel.text! += "\(number)"
+        }
+
+
     }
+    
     
     
     @IBAction func didTapDecimalButton() {
